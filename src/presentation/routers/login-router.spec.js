1b2ffd827572d6a39b57ac66f666888813adcd8c
Emmanuel Ramon <emmanuelrlima@gmail.com>
2021-12-31 14:05:23 -0300

    ensure LoginRouter returns 400 if no password is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
-  route (httRequest) {
-    if (!httRequest.body.email) {
+  route (httpRequest) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -13,6 +13,19 @@ describe('Login Route', () => {
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
+
+describe('Login Route', () => {
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
