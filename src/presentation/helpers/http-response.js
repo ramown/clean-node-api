29dbd08d8ced9f8d9b679ee18e32759eeb247ba0
Emmanuel Ramon <emmanuelrlima@gmail.com>
2022-01-02 15:27:08 -0300

    modify HttpResponse.badRequest to received an error

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,12 +1,11 @@
-const MissingParamError = require('./missing-param-error')
 const ServerError = require('./server-error')
 const UnauthorizedError = require('./unauthorized-error')
 
 module.exports = class HttpResponse {
-  static badRequest (paramName) {
+  static badRequest (error) {
     return {
       statusCode: 400,
-      body: new MissingParamError(paramName)
+      body: error
     }
   }
 
